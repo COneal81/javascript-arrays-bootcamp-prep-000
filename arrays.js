7a2fac09bb3b2a9ab32093773e8439c3049ac0a5
triadfirewood@gmail.com <triadfirewood@gmail.com>
2018-06-21 15:38:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,26 +5,27 @@ var chocolateBars = [
   'skittles'];
   
   function addElementToBeginningOfArray() {
-    chocolateBars.unshift("starburst")
+   ["spree",...chocolateBars];
     return chocolateBars
   }
  
-  function destructivelyAddElementToBeginningOfArray() {["spree",...chocolateBars];
+  function destructivelyAddElementToBeginningOfArray() { chocolateBars.unshift("starburst")
      return chocolateBars
   }
   
   function addElementToEndOfArray() {
-    chocolateBars.push("starburst");
-    return chocolateBars
-  }
-  
-  function destructivelyAddElementToEndOfArray() {
     [...chocolateBars, "spree"]
     return chocolateBars
   }
   
+  function destructivelyAddElementToEndOfArray() {
+    chocolateBars.push("starburst");
+    return chocolateBars
+  }
+  
+  
   function accessElementInArray(){
-    var names = "Kevin", "Kari", "Wyatt";
+    var names = ["Kevin","Kari","Wyatt"];
     return names[2]
   }
   
